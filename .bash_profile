e2410e58fd72363a50f8793b3a633ee251cb9677
cfsanderson <calebsanderson@gmail.com>
2017-07-03 12:52:42 -0400

    deleted setup.sh file

--- .bash_profile
+++ .bash_profile
@@ -1,9 +1,8 @@
-echo "Get to work!"
 
+# Aliases
 alias resource='source $HOME/.bash_profile'
 alias dotconfig='usr/bin/git --git-dir=$HOME/.cfg-dotfiles/ --work-tree=$HOME'
 
 # make zsh the default shell
 export SHELL=`which zsh`
 [ -z "$ZSH_VERSION" ] && exec /bin/zsh -l
-
--- .setup.sh
+++
@@ -1,4 +0,0 @@
-git init --bare $HOME/.cfg-dotfiles
-alias dotconfig='/usr/bin/git --git-dir=$HOME/.cfg-dotfiles/ --work-tree=$HOME'
-dotconfig dotconfig --local status.showUntrackedFiles no
-echo "alias dotconfig='/usr/bin/git --git-dir=$HOME/.cfg-dotfiles/ --work-tree=$HOME'" >> $HOME/.bashrc
