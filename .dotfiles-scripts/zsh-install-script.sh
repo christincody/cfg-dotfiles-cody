61ffad9b646d329b12a7263e27219b74d9501f6a
cfsanderson <calebsanderson@gmail.com>
2017-07-29 20:44:16 -0400

    adding scripts folder

---
+++ .dotfiles-scripts/zsh-install-script.sh
@@ -0,0 +1,10 @@
+zsh --version
+echo "Expected result: 'zsh 4.3.9' or more recent"
+echo $SHELL
+echo "confirm expected result: '/bin/zsh' or similar"
+
+# make it the default shell
+chsh -s $(which zsh)
+echo "===== Log out and login back again to use your new default shell. ====="
+echo "===== Next Step: Install Oh-My-Zsh ====="
+echo "===== curl -Lks https://goo.gl/rysF9G | bash ====="
