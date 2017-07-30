d3507a57ca0b59aba87e0ae09bbf2d821ac66694
cfsanderson <calebsanderson@gmail.com>
2017-07-29 21:10:44 -0400

    updated zsh script

--- .dotfiles-scripts/zsh-install-script.sh
+++ .dotfiles-scripts/zsh-install-script.sh
@@ -3,7 +3,7 @@ echo "Expected result: 'zsh 4.3.9' or more recent"
 echo $SHELL
 echo "confirm expected result: '/bin/zsh' or similar"
 
-# make it the default shell going forward
+# make ZSH the default shell 
 chsh -s $(which zsh)
 echo "===== Log out and login back again to use your new default shell. ====="
 echo "===== Next Step: Install Oh-My-Zsh ====="
