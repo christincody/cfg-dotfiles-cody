3ebf51a56e40798f36ce74cb888de3b64954102e
cfsanderson <calebsanderson@gmail.com>
2017-07-30 21:33:45 -0400

    Adds Xcode install script

---
+++ .dotfiles-scripts/xcode-install-script.sh
@@ -0,0 +1,8 @@
+xcode-select --install
+
+echo "===== xcode-select installed ====="
+echo ""
+echo ""
+echo ""
+echo "Install cfg-dotfiles alias and begin dotfiles setup?"
+echo "===== 'curl -Lks http://bit.do/cfsanderson-cfg-alias | bash' ====="
