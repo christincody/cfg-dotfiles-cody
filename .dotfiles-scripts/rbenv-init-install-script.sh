b46d3c82e2f8e282d5db92454e86f6f0ec4f0ebd
cfsanderson <calebsanderson@gmail.com>
2017-07-30 21:38:01 -0400

    Adds rbenv install script

---
+++ .dotfiles-scripts/rbenv-init-install-script.sh
@@ -0,0 +1,11 @@
+# This script assumes that rbenv has already been installed via Homebrew
+# If rbenv is not already installed then run 'brew update' and 'brew install rbenv' first.
+
+rbenv init
+
+echo "===== manually add '' to the .bash_profile or .zshrc file ====="
+echo ""
+echo ""
+echo ""
+echo "===== Next: Generate new SSH keys and sync with Github ====="
+echo "===== 'curl -Lks http://bit.do/cfsanderson-ssh | bash' ====="
